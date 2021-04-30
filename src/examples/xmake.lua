73057570ac5fbf44b0f3c723998d61138c8c937f
ruki <waruqi@gmail.com>
2021-05-01 00:50:33 +0800

    improve xmake.lua

--- src/examples/xmake.lua
+++ src/examples/xmake.lua
@@ -1,6 +1,9 @@
 for _, filepath in ipairs(os.files(path.join(os.scriptdir(), "*.c"))) do
     local name = path.basename(filepath)
     target(name)
+        if not has_config("examples") then
+            set_default(false)
+        end
         set_kind("binary")
         add_deps("lanox2d")
         add_files(name .. ".c")
--- src/xmake.lua
+++ src/xmake.lua
@@ -1,13 +1,7 @@
-option("tests",    {showmenu = true, default = true, description = "Enable tests"})
-option("examples", {showmenu = true, default = true, description = "Enable exmaples"})
+option("tests",    {showmenu = true, default = false, description = "Enable tests"})
+option("examples", {showmenu = true, default = false, description = "Enable exmaples"})
 option("small",    {showmenu = true, default = true, configvar = {"LX_CONFIG_SMALL", 1},
                     description = "Enable smallest mode and disable all optional modules"})
 option("wchar",    {ctypes = "wchar_t", configvar = {"LX_CONFIG_TYPE_HAVE_WCHAR", 1}})
 
-includes("lanox2d")
-if has_config("tests") then
-    includes("tests")
-end
-if has_config("examples") then
-    includes("examples")
-end
+includes("lanox2d", "tests", "examples")
