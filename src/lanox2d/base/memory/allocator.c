10bd3334013b8a8b071cda43d3fb30044aa970f6
ruki <waruqi@gmail.com>
2021-05-16 16:24:57 +0800

    impl allocator

--- src/lanox2d/base/memory/allocator.c
+++ src/lanox2d/base/memory/allocator.c
@@ -23,10 +23,77 @@
  * includes
  */
 #include "allocator.h"
+#include <stdlib.h>
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * malloc allocator
+ */
+static lx_pointer_t lx_malloc_allocator_malloc(lx_allocator_ref_t allocator, lx_size_t size) {
+    return malloc(size);
+}
+
+static lx_pointer_t lx_malloc_allocator_malloc0(lx_allocator_ref_t allocator, lx_size_t size) {
+    return calloc(1, size);
+}
+
+static lx_pointer_t lx_malloc_allocator_ralloc(lx_allocator_ref_t allocator, lx_pointer_t data, lx_size_t size) {
+    return realloc(data, size);
+}
+
+static lx_void_t lx_malloc_allocator_free(lx_allocator_ref_t allocator, lx_pointer_t data) {
+    free(data);
+}
+
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * globals
+ */
+
+// the malloc allocator
+static lx_allocator_t     g_allocator_malloc = {
+    lx_malloc_allocator_malloc,
+    lx_malloc_allocator_malloc0,
+    lx_malloc_allocator_ralloc,
+    lx_malloc_allocator_free
+};
+
+// the global allocator
+static lx_allocator_ref_t g_allocator = &g_allocator_malloc;
 
 /* //////////////////////////////////////////////////////////////////////////////////////
  * implementation
  */
 
+lx_allocator_ref_t lx_allocator() {
+    return g_allocator;
+}
+
+lx_void_t lx_allocator_set(lx_allocator_ref_t allocator) {
+    g_allocator = allocator;
+}
+
+lx_pointer_t lx_allocator_malloc(lx_allocator_ref_t allocator, lx_size_t size) {
+    return allocator->malloc(allocator, size);
+}
+
+lx_pointer_t lx_allocator_malloc0(lx_allocator_ref_t allocator, lx_size_t size) {
+    return allocator->malloc0(allocator, size);
+}
+
+lx_pointer_t lx_allocator_nalloc(lx_allocator_ref_t allocator, lx_size_t item, lx_size_t size) {
+    return lx_allocator_malloc(allocator, item * size);
+}
+
+lx_pointer_t lx_allocator_nalloc0(lx_allocator_ref_t allocator, lx_size_t item, lx_size_t size) {
+    return lx_allocator_malloc0(allocator, item * size);
+}
+
+lx_pointer_t lx_allocator_ralloc(lx_allocator_ref_t allocator, lx_pointer_t data, lx_size_t size) {
+    return allocator->ralloc(allocator, data, size);
+}
+
+lx_void_t lx_allocator_free(lx_allocator_ref_t allocator, lx_pointer_t data) {
+    allocator->free(allocator, data);
+}
 
 
--- src/lanox2d/base/memory/allocator.h
+++ src/lanox2d/base/memory/allocator.h
@@ -32,8 +32,120 @@
 lx_extern_c_enter
 
 /* //////////////////////////////////////////////////////////////////////////////////////
+ * types
+ */
+
+/// the allocator type
+typedef struct __lx_allocator_t {
+
+    /*! malloc data
+     *
+     * @param allocator     the allocator
+     * @param size          the size
+     *
+     * @return              the data address
+     */
+    lx_pointer_t            (*malloc)(struct __lx_allocator_t* allocator, lx_size_t size);
+
+    /*! malloc data and fill zero
+     *
+     * @param allocator     the allocator
+     * @param size          the size
+     *
+     * @return              the data address
+     */
+    lx_pointer_t            (*malloc0)(struct __lx_allocator_t* allocator, lx_size_t size);
+
+    /*! realloc data
+     *
+     * @param allocator     the allocator
+     * @param data          the data address
+     * @param size          the data size
+     *
+     * @return              the new data address
+     */
+    lx_pointer_t            (*ralloc)(struct __lx_allocator_t* allocator, lx_pointer_t data, lx_size_t size);
+
+    /*! free data
+     *
+     * @param allocator     the allocator
+     * @param data          the data address
+     */
+    lx_void_t               (*free)(struct __lx_allocator_t* allocator, lx_pointer_t data);
+
+}lx_allocator_t, *lx_allocator_ref_t;
+
+/* //////////////////////////////////////////////////////////////////////////////////////
  * interfaces
  */
+
+/*! get the current allocator
+ *
+ * @return              the allocator
+ */
+lx_allocator_ref_t      lx_allocator(lx_noarg_t);
+
+/*! set the current allocator
+ *
+ * @param               the allocator
+ */
+lx_void_t               lx_allocator_set(lx_allocator_ref_t allocator);
+
+/*! malloc data
+ *
+ * @param allocator     the allocator
+ * @param size          the size
+ *
+ * @return              the data address
+ */
+lx_pointer_t            lx_allocator_malloc(lx_allocator_ref_t allocator, lx_size_t size);
+
+/*! malloc data and fill zero
+ *
+ * @param allocator     the allocator
+ * @param size          the size
+ *
+ * @return              the data address
+ */
+lx_pointer_t            lx_allocator_malloc0(lx_allocator_ref_t allocator, lx_size_t size);
+
+/*! malloc data with the item count
+ *
+ * @param allocator     the allocator
+ * @param item          the item count
+ * @param size          the item size
+ *
+ * @return              the data address
+ */
+lx_pointer_t            lx_allocator_nalloc(lx_allocator_ref_t allocator, lx_size_t item, lx_size_t size);
+
+/*! malloc data with the item count and fill zero
+ *
+ * @param allocator     the allocator
+ * @param item          the item count
+ * @param size          the item size
+ *
+ * @return              the data address
+ */
+lx_pointer_t            lx_allocator_nalloc0(lx_allocator_ref_t allocator, lx_size_t item, lx_size_t size);
+
+/*! realloc data
+ *
+ * @param allocator     the allocator
+ * @param data          the data address
+ * @param size          the data size
+ *
+ * @return              the new data address
+ */
+lx_pointer_t            lx_allocator_ralloc(lx_allocator_ref_t allocator, lx_pointer_t data, lx_size_t size);
+
+/*! free data
+ *
+ * @param allocator     the allocator
+ * @param data          the data address
+ *
+ */
+lx_void_t               lx_allocator_free(lx_allocator_ref_t allocator, lx_pointer_t data);
 
 /* //////////////////////////////////////////////////////////////////////////////////////
  * extern
