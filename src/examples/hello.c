8450c2d3d3132883f6bfc381137a42aee6cb7829
ruki <waruqi@gmail.com>
2021-05-18 00:30:59 +0800

    modify title

--- src/examples/hello.c
+++ src/examples/hello.c
@@ -5,7 +5,7 @@ static lx_void_t on_draw(lx_window_ref_t window, lx_canvas_ref_t canvas) {
 }
 
 int main(int argc, char** argv) {
-    lx_window_ref_t window = lx_window_init(640, 480, "Hello Lanox2d");
+    lx_window_ref_t window = lx_window_init(640, 480, "lanox2d");
     if (window) {
         lx_window_on_draw(window, on_draw);
         lx_window_runloop(window);
