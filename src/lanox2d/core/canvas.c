ae6199b5492e611d1cb1c6ab04836b4a1e2f284b
ruki <waruqi@gmail.com>
2021-05-22 00:50:49 +0800

    improve canvas

--- src/lanox2d/core/canvas.c
+++ src/lanox2d/core/canvas.c
@@ -23,14 +23,52 @@
  * includes
  */
 #include "canvas.h"
+#include "device.h"
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * types
+ */
+
+// the canvas type
+typedef struct lx_canvas_t_ {
+    lx_device_ref_t device;
+}lx_canvas_t;
 
 /* //////////////////////////////////////////////////////////////////////////////////////
  * implementation
  */
 lx_canvas_ref_t lx_canvas_init(lx_device_ref_t device) {
-    return lx_null;
+    lx_bool_t    ok = lx_false;
+    lx_canvas_t* canvas = lx_null;
+    do {
+
+        // init canvas
+        canvas = lx_malloc0_type(lx_canvas_t);
+        lx_assert_and_check_break(canvas);
+
+        canvas->device = device;
+
+        ok = lx_true;
+
+    } while (0);
+
+    if (!ok && canvas) {
+        lx_canvas_exit((lx_canvas_ref_t)canvas);
+        canvas = lx_null;
+    }
+    return (lx_canvas_ref_t)canvas;
 }
 
-lx_void_t lx_canvas_exit(lx_canvas_ref_t canvas) {
+lx_void_t lx_canvas_exit(lx_canvas_ref_t self) {
+    lx_canvas_t* canvas = (lx_canvas_t*)self;
+    if (canvas) {
+        lx_free(canvas);
+    }
 }
 
+lx_void_t lx_canvas_draw_clear(lx_canvas_ref_t self, lx_color_t color) {
+    lx_canvas_t* canvas = (lx_canvas_t*)self;
+    if (canvas && canvas->device) {
+        lx_device_draw_clear(canvas->device, color);
+    }
+}
