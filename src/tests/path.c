b98819e9530a47159ce7f20d85a1b630cdc82a72
ruki <waruqi@gmail.com>
2021-06-08 23:37:23 +0800

    add path test

---
+++ src/tests/path.c
@@ -0,0 +1,25 @@
+#include "lanox2d/lanox2d.h"
+
+int main(int argc, char** argv) {
+    lx_path_ref_t path = lx_path_init();
+    if (path) {
+        // init path
+        lx_path_move2i_to(path, 100, 100);
+        lx_path_line2i_to(path, 200, 200);
+        lx_path_quad2i_to(path, 300, 300, 400, 400);
+        lx_path_cubic2i_to(path, 500, 500, 600, 600, 700, 700);
+        lx_path_close(path);
+
+        // copy path
+        lx_path_ref_t copy = lx_path_init();
+        if (copy) {
+            lx_path_copy(copy, path);
+#ifdef LX_DEBUG
+//            lx_path_dump(copy);
+#endif
+            lx_path_exit(copy);
+        }
+        lx_path_exit(path);
+    }
+    return 0;
+}
