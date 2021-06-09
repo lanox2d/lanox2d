df3b37f31adaf1280a9c30423609a155d8ab6fcd
ruki <waruqi@gmail.com>
2021-06-10 00:37:02 +0800

    improve test

--- src/tests/stack.c
+++ src/tests/stack.c
@@ -4,8 +4,9 @@ static lx_void_t itemfree(lx_pointer_t item) {
     lx_trace_i("free: %lu", *((lx_size_t*)item));
 }
 
-static lx_void_t foreach(lx_pointer_t item, lx_cpointer_t udata) {
+static lx_bool_t foreach(lx_iterator_ref_t iterator, lx_pointer_t item, lx_cpointer_t udata) {
     lx_trace_i("foreach: %lu", *((lx_size_t*)item));
+    return lx_true;
 }
 
 int main(int argc, char** argv) {
@@ -26,10 +27,10 @@ int main(int argc, char** argv) {
         lx_assert(*((lx_size_t*)lx_stack_head(stack)) == val[0]);
         lx_assert(*((lx_size_t*)lx_stack_last(stack)) == val[9]);
         lx_assert(*((lx_size_t*)lx_stack_top(stack)) == val[9]);
-        lx_stack_foreach(stack, foreach, lx_null);
+        lx_foreach_all(stack, foreach, lx_null);
         lx_trace_i("-------------------------- test pop ---------------------------");
         lx_stack_pop(stack);
-        lx_stack_foreach(stack, foreach, lx_null);
+        lx_foreach_all(stack, foreach, lx_null);
         lx_assert(*((lx_size_t*)lx_stack_top(stack)) == val[8]);
         lx_stack_exit(stack);
     }
