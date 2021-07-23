5cf6a2c2111e34a9128e456c83f3a2c873dc4e38
ruki <waruqi@gmail.com>
2021-07-24 00:45:48 +0800

    improve examples

--- src/examples/shape/triangle.c
+++ src/examples/shape/triangle.c
@@ -6,9 +6,11 @@ static lx_void_t on_draw_triangle(lx_window_ref_t window, lx_canvas_ref_t canvas
 
     lx_canvas_color_set(canvas, LX_COLOR_RED);
     lx_canvas_mode_set(canvas, LX_PAINT_MODE_FILL);
+    lx_canvas_shader_set(canvas, g_shaders[g_shader]);
     lx_canvas_draw_triangle(canvas, &triangle);
 
     lx_canvas_color_set(canvas, LX_COLOR_BLUE);
     lx_canvas_mode_set(canvas, LX_PAINT_MODE_STROKE);
+    lx_canvas_shader_set(canvas, lx_null);
     lx_canvas_draw_triangle(canvas, &triangle);
 }
