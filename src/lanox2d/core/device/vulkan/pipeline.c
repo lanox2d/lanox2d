dd0766e144d62283dda6ea02c4b015dc3d96f300
ruki <waruqi@gmail.com>
2021-10-21 00:34:14 +0800

    create pipeline

--- src/lanox2d/core/device/vulkan/pipeline.c
+++ src/lanox2d/core/device/vulkan/pipeline.c
@@ -57,12 +57,12 @@ lx_pipeline_ref_t lx_pipeline_init(lx_vulkan_device_t* device, lx_size_t type, l
 
         // create pipeline layout (empty)
         VkPipelineLayoutCreateInfo pipeline_layout_createinfo = {};
-        pipeline_layout_createinfo.sType                  = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-        pipeline_layout_createinfo.pNext                  = lx_null;
-        pipeline_layout_createinfo.setLayoutCount         = 0;
-        pipeline_layout_createinfo.pSetLayouts            = lx_null;
+        pipeline_layout_createinfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+        pipeline_layout_createinfo.pNext = lx_null;
+        pipeline_layout_createinfo.setLayoutCount = 0;
+        pipeline_layout_createinfo.pSetLayouts = lx_null;
         pipeline_layout_createinfo.pushConstantRangeCount = 0;
-        pipeline_layout_createinfo.pPushConstantRanges    = lx_null;
+        pipeline_layout_createinfo.pPushConstantRanges = lx_null;
         if (vkCreatePipelineLayout(device->device, &pipeline_layout_createinfo, lx_null, &pipeline->pipeline_layout) != VK_SUCCESS) {
             break;
         }
@@ -86,6 +86,148 @@ lx_pipeline_ref_t lx_pipeline_init(lx_vulkan_device_t* device, lx_size_t type, l
         fshader_createinfo.codeSize = (lx_uint32_t)lx_strlen(fshader);
         fshader_createinfo.pCode = (lx_uint32_t*)fshader;
         if (vkCreateShaderModule(device->device, &fshader_createinfo, lx_null, &fshader_module) != VK_SUCCESS) {
+            break;
+        }
+
+        // init vertex and fragment shader stages
+        VkPipelineShaderStageCreateInfo shader_stages[2] = {};
+        shader_stages[0].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        shader_stages[0].pNext = lx_null;
+        shader_stages[0].flags = 0;
+        shader_stages[0].stage = VK_SHADER_STAGE_VERTEX_BIT;
+        shader_stages[0].module = vshader_module;
+        shader_stages[0].pName = "main";
+        shader_stages[0].pSpecializationInfo = lx_null;
+
+        shader_stages[1].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        shader_stages[1].pNext = lx_null;
+        shader_stages[1].flags = 0;
+        shader_stages[1].stage = VK_SHADER_STAGE_VERTEX_BIT;
+        shader_stages[1].module = fshader_module;
+        shader_stages[1].pName = "main";
+        shader_stages[1].pSpecializationInfo = lx_null;
+
+        // init viewport
+        VkViewport viewport = {};
+        viewport.x = 0;
+        viewport.y = 0;
+        viewport.width = (lx_float_t)device->framesize.width;
+        viewport.height = (lx_float_t)device->framesize.height;
+        viewport.minDepth = 0.0f;
+        viewport.maxDepth = 1.0f;
+
+        VkRect2D scissor = {};
+        scissor.offset.x = 0;
+        scissor.offset.y = 0;
+        scissor.extent = device->framesize;
+
+        VkPipelineViewportStateCreateInfo viewport_info = {};
+        viewport_info.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+        viewport_info.pNext = lx_null;
+        viewport_info.viewportCount = 1;
+        viewport_info.pViewports = &viewport;
+        viewport_info.scissorCount = 1;
+        viewport_info.pScissors = &scissor;
+
+        // init multisample info
+        VkSampleMask sample_mask = ~0u;
+        VkPipelineMultisampleStateCreateInfo multisample_info = {};
+        multisample_info.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+        multisample_info.pNext = lx_null;
+        multisample_info.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+        multisample_info.sampleShadingEnable = VK_FALSE;
+        multisample_info.minSampleShading = 0;
+        multisample_info.pSampleMask = &sample_mask;
+        multisample_info.alphaToCoverageEnable = VK_FALSE;
+        multisample_info.alphaToOneEnable = VK_FALSE;
+
+        // init color blend state
+        VkPipelineColorBlendAttachmentState attachment_states = {};
+        attachment_states.blendEnable = VK_FALSE;
+        attachment_states.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT |
+                                           VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+
+        VkPipelineColorBlendStateCreateInfo color_blendinfo = {};
+        color_blendinfo.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+        color_blendinfo.pNext = lx_null;
+        color_blendinfo.flags = 0;
+        color_blendinfo.logicOpEnable = VK_FALSE;
+        color_blendinfo.logicOp = VK_LOGIC_OP_COPY;
+        color_blendinfo.attachmentCount = 1;
+        color_blendinfo.pAttachments = &attachment_states;
+
+        // init rasterizer info
+        VkPipelineRasterizationStateCreateInfo raster_info = {};
+        raster_info.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+        raster_info.pNext = lx_null;
+        raster_info.depthClampEnable = VK_FALSE;
+        raster_info.rasterizerDiscardEnable = VK_FALSE;
+        raster_info.polygonMode = VK_POLYGON_MODE_FILL;
+        raster_info.cullMode = VK_CULL_MODE_NONE;
+        raster_info.frontFace = VK_FRONT_FACE_CLOCKWISE;
+        raster_info.depthBiasEnable = VK_FALSE;
+        raster_info.lineWidth = 1;
+
+        // init input assembler state
+        VkPipelineInputAssemblyStateCreateInfo input_assemblyinfo = {};
+        input_assemblyinfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+        input_assemblyinfo.pNext = lx_null;
+        input_assemblyinfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+        input_assemblyinfo.primitiveRestartEnable = VK_FALSE;
+
+        // init vertex input state
+        VkVertexInputBindingDescription vertex_input_bindings = {};
+        vertex_input_bindings.binding = 0;
+        vertex_input_bindings.stride = 3 * sizeof(lx_float_t);
+        vertex_input_bindings.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+        VkVertexInputAttributeDescription vertex_input_attributes[1];
+        vertex_input_attributes[0].location = 0;
+        vertex_input_attributes[0].binding = 0;
+        vertex_input_attributes[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+        vertex_input_attributes[0].offset = 0;
+
+        VkPipelineVertexInputStateCreateInfo vertex_inputinfo = {};
+        vertex_inputinfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+        vertex_inputinfo.pNext = lx_null;
+        vertex_inputinfo.vertexBindingDescriptionCount = 1;
+        vertex_inputinfo.pVertexBindingDescriptions = &vertex_input_bindings;
+        vertex_inputinfo.vertexAttributeDescriptionCount = 1;
+        vertex_inputinfo.pVertexAttributeDescriptions = vertex_input_attributes;
+
+        // create the pipeline cache
+        VkPipelineCacheCreateInfo pipeline_cacheinfo = {};
+        pipeline_cacheinfo.sType = VK_STRUCTURE_TYPE_PIPELINE_CACHE_CREATE_INFO;
+        pipeline_cacheinfo.pNext = lx_null;
+        pipeline_cacheinfo.flags = 0; // reserved, must be 0
+        pipeline_cacheinfo.initialDataSize = 0;
+        pipeline_cacheinfo.pInitialData = lx_null;
+        if (vkCreatePipelineCache(device->device, &pipeline_cacheinfo, lx_null, &pipeline->pipeline_cache) != VK_SUCCESS) {
+            break;
+        }
+
+        // create the pipeline
+        VkGraphicsPipelineCreateInfo pipeline_createinfo = {};
+        pipeline_createinfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+        pipeline_createinfo.pNext = lx_null;
+        pipeline_createinfo.flags = 0;
+        pipeline_createinfo.stageCount = 2;
+        pipeline_createinfo.pStages = shader_stages;
+        pipeline_createinfo.pVertexInputState = &vertex_inputinfo;
+        pipeline_createinfo.pInputAssemblyState = &input_assemblyinfo;
+        pipeline_createinfo.pTessellationState = lx_null;
+        pipeline_createinfo.pViewportState = &viewport_info;
+        pipeline_createinfo.pRasterizationState = &raster_info;
+        pipeline_createinfo.pMultisampleState = &multisample_info;
+        pipeline_createinfo.pDepthStencilState = lx_null;
+        pipeline_createinfo.pColorBlendState = &color_blendinfo;
+        pipeline_createinfo.pDynamicState = lx_null;
+        pipeline_createinfo.layout = pipeline->pipeline_layout;
+        pipeline_createinfo.renderPass = device->renderpass;
+        pipeline_createinfo.subpass = 0;
+        pipeline_createinfo.basePipelineHandle = VK_NULL_HANDLE;
+        pipeline_createinfo.basePipelineIndex = 0;
+        if (vkCreateGraphicsPipelines(device->device, pipeline->pipeline_cache, 1, &pipeline_createinfo, lx_null, &pipeline->pipeline) != VK_SUCCESS) {
             break;
         }
 
