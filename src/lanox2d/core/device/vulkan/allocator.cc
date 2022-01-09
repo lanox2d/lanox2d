5e121e1f7743861268b1dad5a75b863f8b8f11f7
ruki <waruqi@gmail.com>
2022-01-09 22:44:17 +0800

    improve vma buffer

--- src/lanox2d/core/device/vulkan/allocator.cc
+++ src/lanox2d/core/device/vulkan/allocator.cc
@@ -45,6 +45,14 @@ typedef struct lx_vk_allocator_t {
     VmaAllocator            allocator;
 }lx_vk_allocator_t;
 
+// the vulkan/vma buffer type
+typedef struct lx_vk_vma_buffer_t_ {
+    VkBuffer        buffer;
+    lx_size_t       offset;
+    lx_size_t       size;
+    VmaAllocation   allocation;
+}lx_vk_vma_buffer_t;
+
 /* //////////////////////////////////////////////////////////////////////////////////////
  * implementation
  */
@@ -69,7 +77,7 @@ lx_vk_allocator_ref_t lx_vk_allocator_init(lx_vulkan_device_t* device, VkBufferU
         if (vmaCreateAllocator(&allocator_info, &allocator->allocator) != VK_SUCCESS) {
             break;
         }
-        lx_assert_static(sizeof(VmaAllocation) <= lx_memsizeof(lx_vk_buffer_t, privdata));
+        lx_assert_static(sizeof(lx_vk_vma_buffer_t) <= sizeof(lx_vk_buffer_t));
 
         ok = lx_true;
     } while (0);
@@ -90,33 +98,43 @@ lx_void_t lx_vk_allocator_exit(lx_vk_allocator_ref_t self) {
 
 lx_bool_t lx_vk_allocator_alloc(lx_vk_allocator_ref_t self, lx_size_t size, lx_vk_buffer_t* buffer) {
     lx_vk_allocator_t* allocator = (lx_vk_allocator_t*)self;
-    lx_assert(allocator && size && buffer);
+    lx_vk_vma_buffer_t* vma_buffer = (lx_vk_vma_buffer_t*)buffer;
+    lx_assert(allocator && size && vma_buffer);
 
     VkBufferCreateInfo buffer_info = {};
     buffer_info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
     buffer_info.size = size;
     buffer_info.usage = allocator->buffer_type;
 
-    VmaAllocationCreateInfo vmaalloc_info = {};
-    vmaalloc_info.usage = VMA_MEMORY_USAGE_CPU_TO_GPU;
-    return vmaCreateBuffer(allocator->allocator, &buffer_info, &vmaalloc_info, &buffer->buffer, (VmaAllocation*)buffer->privdata, lx_null) == VK_SUCCESS;
+    VmaAllocationCreateInfo vma_alloc_createinfo = {};
+    vma_alloc_createinfo.usage = VMA_MEMORY_USAGE_CPU_TO_GPU;
+    return vmaCreateBuffer(allocator->allocator, &buffer_info, &vma_alloc_createinfo, &vma_buffer->buffer, &vma_buffer->allocation, lx_null) == VK_SUCCESS;
 }
 
 lx_void_t lx_vk_allocator_free(lx_vk_allocator_ref_t self, lx_vk_buffer_t* buffer) {
     lx_vk_allocator_t* allocator = (lx_vk_allocator_t*)self;
-    lx_assert(allocator && buffer);
+    lx_vk_vma_buffer_t* vma_buffer = (lx_vk_vma_buffer_t*)buffer;
+    lx_assert(allocator && vma_buffer);
 
-    vmaDestroyBuffer(allocator->allocator, buffer->buffer, *((VmaAllocation*)buffer->privdata));
+    vmaDestroyBuffer(allocator->allocator, vma_buffer->buffer, vma_buffer->allocation);
 }
 
 lx_void_t lx_vk_allocator_copy(lx_vk_allocator_ref_t self, lx_vk_buffer_t* buffer, lx_pointer_t data, lx_size_t size) {
     lx_vk_allocator_t* allocator = (lx_vk_allocator_t*)self;
-    lx_assert(allocator && buffer && data && size);
+    lx_vk_vma_buffer_t* vma_buffer = (lx_vk_vma_buffer_t*)buffer;
+    lx_assert(allocator && vma_buffer && vma_buffer->allocation && data && size);
+
+#ifdef LX_DEBUG
+    VmaAllocationInfo vma_allocinfo;
+    vmaGetAllocationInfo(allocator->allocator, vma_buffer->allocation, &vma_allocinfo);
+    lx_assert(size <= vma_allocinfo.size);
+    lx_trace_i("%lu %lu %lu", vma_allocinfo.offset, vma_allocinfo.size, size);
+#endif
 
     lx_pointer_t buffer_data = lx_null;
-    vmaMapMemory(allocator->allocator, *((VmaAllocation*)buffer->privdata), &buffer_data);
+    vmaMapMemory(allocator->allocator, vma_buffer->allocation, &buffer_data);
     if (buffer_data) {
         lx_memcpy(buffer_data, data, size);
-        vmaUnmapMemory(allocator->allocator, *((VmaAllocation*)buffer->privdata));
+        vmaUnmapMemory(allocator->allocator, vma_buffer->allocation);
     }
 }
