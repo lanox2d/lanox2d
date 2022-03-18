bb0ee56bc6a48f7d3547ac5733af78ef889e438d
ruki <waruqi@gmail.com>
2022-03-19 00:26:49 +0800

    grow descriptor pools

--- src/lanox2d/core/device/vulkan/descriptor_sets.c
+++ src/lanox2d/core/device/vulkan/descriptor_sets.c
@@ -29,13 +29,15 @@
  * macros
  */
 #ifdef LX_CONFIG_SMALL
-#   define LX_VK_DESCRIPTOR_FREE_SETS_GROW      (16)
-#   define LX_VK_DESCRIPTOR_INIT                (16)
-#   define LX_VK_DESCRIPTOR_MAXN                (1024)
+#   define LX_VK_DESCRIPTOR_FREE_SETS_GROW          (16)
+#   define LX_VK_DESCRIPTOR_DESCRIPTOR_POOLS_GROW   (8)
+#   define LX_VK_DESCRIPTOR_INIT                    (16)
+#   define LX_VK_DESCRIPTOR_MAXN                    (1024)
 #else
-#   define LX_VK_DESCRIPTOR_FREE_SETS_GROW      (64)
-#   define LX_VK_DESCRIPTOR_INIT                (32)
-#   define LX_VK_DESCRIPTOR_MAXN                (2048)
+#   define LX_VK_DESCRIPTOR_FREE_SETS_GROW          (64)
+#   define LX_VK_DESCRIPTOR_DESCRIPTOR_POOLS_GROW   (32)
+#   define LX_VK_DESCRIPTOR_INIT                    (32)
+#   define LX_VK_DESCRIPTOR_MAXN                    (2048)
 #endif
 
 /* //////////////////////////////////////////////////////////////////////////////////////
@@ -48,21 +50,68 @@ typedef struct lx_vk_descriptor_sets_t {
     VkDescriptorType            descriptor_type;
     VkDescriptorSetLayout       descriptor_set_layout;
     lx_uint32_t                 descriptor_count_per_set;
+    lx_uint32_t                 descriptor_count_current;
+    lx_uint32_t                 descriptor_maxn;
     lx_array_ref_t              free_sets;
-    lx_vk_descriptor_pool_ref_t descriptor_pool;
+    lx_array_ref_t              descriptor_pools;
 }lx_vk_descriptor_sets_t;
 
 /* //////////////////////////////////////////////////////////////////////////////////////
  * implementation
  */
 
+static lx_vk_descriptor_pool_ref_t lx_vk_descriptor_sets_get_new_descriptor_pool(lx_vk_descriptor_sets_t* descriptor_sets) {
+    lx_vulkan_device_t* device = descriptor_sets->device;
+    if (lx_array_size(descriptor_sets->descriptor_pools)) {
+        lx_uint32_t pool_size = descriptor_sets->descriptor_maxn + ((descriptor_sets->descriptor_maxn + 1) >> 1);
+        if (pool_size < LX_VK_DESCRIPTOR_MAXN) {
+            descriptor_sets->descriptor_maxn = pool_size;
+        } else {
+            descriptor_sets->descriptor_maxn = LX_VK_DESCRIPTOR_MAXN;
+        }
+
+    }
+    lx_vk_descriptor_pool_ref_t descriptor_pool = lx_vk_descriptor_pool_init(device, descriptor_sets->descriptor_type, descriptor_sets->descriptor_maxn);
+    if (descriptor_pool) {
+        lx_array_insert_tail(descriptor_sets->descriptor_pools, &descriptor_pool);
+    }
+    return descriptor_pool;
+}
+
+static VkDescriptorPool lx_vk_descriptor_sets_get_descriptor_pool(lx_vk_descriptor_sets_t* descriptor_sets) {
+    if (lx_array_size(descriptor_sets->descriptor_pools)) {
+        lx_vk_descriptor_pool_ref_t* pdescriptor_pool = (lx_vk_descriptor_pool_ref_t*)lx_array_last(descriptor_sets->descriptor_pools);
+        if (pdescriptor_pool) {
+            lx_vk_descriptor_pool_ref_t descriptor_pool = *pdescriptor_pool;
+            if (descriptor_pool) {
+                return lx_vk_descriptor_pool(descriptor_pool);
+            }
+        }
+    }
+    return VK_NULL_HANDLE;
+}
+
+static lx_void_t lx_vk_descriptor_sets_free_descriptor_pool(lx_pointer_t item, lx_pointer_t udata) {
+    lx_vk_descriptor_pool_ref_t* pdescriptor_pool = (lx_vk_descriptor_pool_ref_t*)item;
+    if (pdescriptor_pool && *pdescriptor_pool) {
+        lx_vk_descriptor_pool_exit(*pdescriptor_pool);
+    }
+}
+
 static VkDescriptorSet lx_vk_descriptor_sets_get_new_descriptor_set(lx_vk_descriptor_sets_t* descriptor_sets) {
     lx_vulkan_device_t* device = descriptor_sets->device;
-    lx_assert(device);
+    lx_assert(device && descriptor_sets->descriptor_maxn);
 
     // get the descriptor pool
-    VkDescriptorPool descriptor_pool = lx_vk_descriptor_pool(descriptor_sets->descriptor_pool);
-    lx_assert(descriptor_pool);
+    descriptor_sets->descriptor_count_current += descriptor_sets->descriptor_count_per_set;
+    if (!lx_array_size(descriptor_sets->descriptor_pools) || descriptor_sets->descriptor_count_current > descriptor_sets->descriptor_maxn) {
+        if (!lx_vk_descriptor_sets_get_new_descriptor_pool(descriptor_sets)) {
+            return VK_NULL_HANDLE;
+        }
+        descriptor_sets->descriptor_count_current = descriptor_sets->descriptor_count_per_set;
+    }
+    VkDescriptorPool descriptor_pool = lx_vk_descriptor_sets_get_descriptor_pool(descriptor_sets);
+    lx_assert(descriptor_pool != VK_NULL_HANDLE);
 
     // create a descriptor set
     VkDescriptorSet descriptor_set = VK_NULL_HANDLE;
@@ -122,6 +171,7 @@ static lx_vk_descriptor_sets_ref_t lx_vk_descriptor_sets_init(lx_vulkan_device_t
 
         descriptor_sets->device = device;
         descriptor_sets->descriptor_type = type;
+        descriptor_sets->descriptor_maxn = LX_VK_DESCRIPTOR_INIT;
 
         descriptor_sets->descriptor_count_per_set = lx_vk_descriptor_sets_get_layout_and_set_count(device,
             type, stages, stages_size, &descriptor_sets->descriptor_set_layout);
@@ -130,8 +180,9 @@ static lx_vk_descriptor_sets_ref_t lx_vk_descriptor_sets_init(lx_vulkan_device_t
         descriptor_sets->free_sets = lx_array_init(LX_VK_DESCRIPTOR_FREE_SETS_GROW, lx_element_mem(sizeof(VkDescriptorSet), lx_null, lx_null));
         lx_assert_and_check_break(descriptor_sets->free_sets);
 
-        descriptor_sets->descriptor_pool = lx_vk_descriptor_pool_init(device, type, LX_VK_DESCRIPTOR_INIT);
-        lx_assert_and_check_break(descriptor_sets->descriptor_pool);
+        descriptor_sets->descriptor_pools = lx_array_init(LX_VK_DESCRIPTOR_DESCRIPTOR_POOLS_GROW,
+            lx_element_mem(sizeof(lx_vk_descriptor_pool_ref_t), lx_vk_descriptor_sets_free_descriptor_pool, lx_null));
+        lx_assert_and_check_break(descriptor_sets->descriptor_pools);
 
         ok = lx_true;
     } while (0);
@@ -168,9 +219,9 @@ lx_void_t lx_vk_descriptor_sets_exit(lx_vk_descriptor_sets_ref_t self) {
             descriptor_sets->descriptor_set_layout = VK_NULL_HANDLE;
         }
 
-        if (descriptor_sets->descriptor_pool) {
-            lx_vk_descriptor_pool_exit(descriptor_sets->descriptor_pool);
-            descriptor_sets->descriptor_pool = lx_null;
+        if (descriptor_sets->descriptor_pools) {
+            lx_array_exit(descriptor_sets->descriptor_pools);
+            descriptor_sets->descriptor_pools = lx_null;
         }
         lx_free(descriptor_sets);
     }
